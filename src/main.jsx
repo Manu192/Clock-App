afec71b5a9620f7433c3ba45dd692d5f1d2efa00
Manu192 <jksmrithy1@gmail.com>
2025-11-08 14:33:39 +0530

    final changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ClockProvider } from './Context/Clockcontext'; 
 import './index.css';
+import { ClockProvider } from './context/ClockContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
