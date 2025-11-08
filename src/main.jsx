ad38913816394b2b42e24e22eda4417555820993
Manu192 <jksmrithy1@gmail.com>
2025-11-08 14:21:32 +0530

    changes made

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ClockProvider } from './Context/ClockContext'; 
+import { ClockProvider } from './Context/Clockcontext'; 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
